77b93dddaf3b5330d15e46a114d5fd26e16fbde3
Christian Henriksen <christian@utopians.dk>
2017-03-02 22:22:56 +0100

    Add new documents

--- routers/DocumentRouter.js
+++ routers/DocumentRouter.js
@@ -6,7 +6,7 @@ module.exports = (state) => {
     router.get("/", (request, response) => {
         const databaseName = request.query.database;
         const collectionName = request.query.collection;
-        console.log("/collections - GET - " + databaseName + " : " + collectionName);
+        console.log("/documents - GET - " + databaseName + " : " + collectionName);
 
         const database = state.database.db(databaseName);
         const stream = database.collection(collectionName).find().stream();
@@ -22,7 +22,31 @@ module.exports = (state) => {
         });
         stream.on('end', function() {
             const title = databaseName + " - " + collectionName;
-            return response.render("documents", { title: title, documents: documents });
+            return response.render("documents", { title: title, documents: documents, database: databaseName, collection: collectionName });
+        });
+    });
+
+    router.get("/new", (request, response) => {
+        const databaseName = request.query.database;
+        const collectionName = request.query.collection;
+
+        const title = databaseName + " - " + collectionName + " - " + "New document";
+        return response.render("newDocument", { title: title, database: databaseName, collection: collectionName });
+    });
+
+    router.post("/", (request, response) => {
+        const databaseName = request.query.database;
+        const collectionName = request.query.collection;
+        console.log("/documents - POST - " + databaseName + " - " + collectionName);
+
+        const newDocument = request.body;
+        
+        const database = state.database.db(databaseName);
+        database.collection(collectionName).insertOne(newDocument, (error, result) => {
+            if ( error ) console.log("Error saving document - " + error);
+
+            const url = "/documents" + "?database=" + databaseName + "&collection=" + collectionName;
+            return response.redirect(301, url);
         });
     });
 
